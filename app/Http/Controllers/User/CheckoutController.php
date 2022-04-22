9e5fc18ef3c0f597b77579baa3d3f479db9201b9
virgo veronica <virgoveronika1@gmail.com>
2022-04-22 16:56:28 +0700

    validation landing page & validation

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use App\Models\Checkout;
 use Illuminate\Http\Request;
+use App\Http\Requests\User\Checkout\Store;
 use App\Models\Camp;
 use Auth;
 
@@ -25,9 +26,13 @@ class CheckoutController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create(Camp $camp)
+    public function create(Camp $camp, Request $request)
     {
-        
+       if ($camp->isRegistered) {
+        $request->session()->flash('error', "You Already registered on {$camp->title} camp.");
+        return redirect(route('dashboard'));
+           
+       } 
         return view('checkout.create', [
             'camp' => $camp
         ]);
@@ -39,9 +44,9 @@ class CheckoutController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request, Camp $camp)
+    public function store(Store $request, Camp $camp)
     {
-        
+        return $request->all();
         // mapping request data
         $data =$request->all();
         $data['user_id'] = Auth::id();
--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -15,6 +15,7 @@
                 </div>
             </div>
             <div class="row my-5">
+                @include ('components.alert')
                 <table class="table">
                     <tbody>
                     @forelse ($checkouts as $checkout)
