393334182fb6813567877195acf29e12c50314d2
virgo veronica <virgoveronika1@gmail.com>
2022-05-02 18:42:51 +0700

    update dashboard  admin and user

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -35,11 +35,12 @@
                                 <strong>${{$checkout->Camp->price}}k</strong>
                             </td>
                             <td>
-                                @if ($checkout->is_paid)
-                                <strong class="text-success">Payment Success</strong>
-                                @else 
-                                <strong>Waiting for Payment</strong>
-                                @endif
+                                <strong>{{$checkout->payment_status}}</strong>
+                            </td>
+                            <td>
+                               @if ($checkout->payment_status == 'waiting')
+                               <a href="{{$checkout->midtrans_url}}" class="btn btn-primary">Pay Here</a>
+                               @endif
                             </td>
                             <td>
                                 <a href="https://wa.me/089503214304?text=Hi, saya ingin bertanya tentang kelas {{$checkout->Camp->title}}" class="btn btn-primary">
@@ -50,7 +51,7 @@
                         @empty
                         <tr>
                             <td colspan="5">
-                                <h3>No Data</h3>
+                                <h3>No Camp Registered</h3>
                             </td>
                         </tr>
                         @endforelse
